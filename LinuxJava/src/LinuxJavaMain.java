c4c44754f4ce1a56eb9c363712a7060ba45a5f2b
BIT <BIT@DESKTOP-DOGVD1T>
2018-03-28 15:22:29 +0900

    2

--- LinuxJava/src/LinuxJavaMain.java
+++ LinuxJava/src/LinuxJavaMain.java
@@ -4,7 +4,7 @@ public class LinuxJavaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello linux");
-
+		System.out.println("2 hello comit linux");
 	}
 
 }
